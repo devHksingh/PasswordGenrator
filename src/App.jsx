c46aececa4ea7c04923f5e75a82040a3beb59f86
devHksingh <hksingh.dev@gmail.com>
2023-09-19 20:18:02 +0530

    added functionality of code react and js part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
+import { useCallback, useState,useEffect, useRef } from 'react'
 
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setLength] = useState(6);
+  const [numberAllowed,setNumberAllowed] = useState(false)
+  const [upperCharAllowed,setUpperCharAllowed] = useState(false)
+  const [lowerCharAllowed,setLowerCharPassword] = useState(false)
+  const [symbolsAllowed,setSymbolsAllowed] = useState(false)
+  const [password,setPassword] = useState("")
+
+  // useRef hook
+  const passwordRef = useRef(null)
+
+  //callback
+
+  const passwordGenerator = useCallback(()=>{
+
+    let pass =""
+    let str =""
+
+    if(numberAllowed) str+="0123456789"
+    if(upperCharAllowed) str+= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(lowerCharAllowed) str+= "abcdefghijklmnopqrstuvwxyz"
+    if(symbolsAllowed) str+=  "~!@#$%&*()_+*?"
+
+    for (let i = 0; i <= length; i++) {
+      let indexValue = Math.floor(Math.random()*str.length+1)
+      pass += str.charAt(indexValue)
+      
+    }
+
+    setPassword(pass)
+
+  },[length,numberAllowed,upperCharAllowed,lowerCharAllowed,symbolsAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(()=>{
+      password.current?.select()
+      password.current?.setSelectionRange(0,40)
+      window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,upperCharAllowed,lowerCharAllowed,symbolsAllowed,passwordGenerator])
 
   return (
     <>
-      <h1 className='text-center font-black text-4xl text bg-grayish-200 font-jetbrainsmono'>PassWord genrator</h1>
-      <h1 className='text-center font-jetbrainsmono italic'>Lorem ipsum dolor sit amet consectetur.</h1>
+      
     </>
   )
 }
